d81adc3a0307a880cb44120d579f7815cfb08561
Akwill47 <wil.weat@hotmail.com>
2021-05-30 11:05:51 -0400

    successfully ran the output

--- assignment1.c
+++ assignment1.c
@@ -19,7 +19,7 @@ course *read_courses(FILE *fp, int *num_courses);
 student **read_sections(FILE *fp, int num_students[], int num_scores[], int num_sections);
 void process_courses(course *courses, int num_courses);
 void release_courses( course *courses, int num_courses);
-
+//we did it
 int main(void){
   FILE *fp = fopen ("assignment1input.txt","r");
   int t;
